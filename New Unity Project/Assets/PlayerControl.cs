0c35fcce54592b9093e9de821ec425f41e18895c
細田 <glasia893@i.softbank.jp>
2020-02-20 14:16:22 +0900

    prototype

---
+++ New Unity Project/Assets/PlayerControl.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 100.0f;
+    public float JunmpPower = 700.0f;
+    Rigidbody2D rigidbody2d;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        Vector2 Position = transform.position;
+       // float x = Input.GetAxis("Horizontal");
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            Position.x += speed;
+            //rigidbody2d.AddForce(transform.right * speed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Position.x -= speed;
+            //rigidbody2d.AddForce(transform.right * -speed);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2d.AddForce(transform.up *JunmpPower);
+        }
+        transform.position = Position;
+    }
+}
---
+++ New Unity Project/Assets/WaterMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMove : MonoBehaviour
+{
+    bool isUp = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isUp == true)
+        {
+            transform.localScale += new Vector3(0, 0.01f, 0);
+        }
+        else
+        {
+            transform.localScale += new Vector3(0, -0.01f, 0);
+        }
+
+        if (transform.localScale.y >= 1.7)
+        {
+            isUp = false;
+        }
+        
+        if(transform.localScale.y <= 0.5)
+        {
+            isUp = true;
+        }
+    }
+}
